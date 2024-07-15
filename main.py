8b6bea546ce2558b4f392f49dd86aa395cf2dbc1
TickTockBent <benttick@gmail.com>
2024-07-15 09:37:29 -0400

    intents

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Create bot instance
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Load cogs
