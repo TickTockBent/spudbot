792b822086f358419e80ee4bbde796800c194ab6
TickTockBent <benttick@gmail.com>
2024-07-15 09:32:50 -0400

    fill in channel names

--- config.py
+++ config.py
@@ -12,11 +12,17 @@ API_KEY = 'e981bf13-3715-498c-ae32-dbd1301cb9c5'
 
 # Channel IDs to be updated (placeholders for now)
 CHANNEL_IDS = {
-    'epoch': 123456789,
-    'layer': 234567890,
-    'smeshers': 345678901,
-    'rewards': 456789012,
+    'price': 1262398089830858823,
+    'circulatingsupply': 1262397961086963814,
+    'marketcap': 1262398109921837068,
+    'epoch': 1262398146072412200,
+    'layer': 1262398059875270656,
+    'networksize': 1262398171191967838,
+    'activesmeshers': 1262397844174798939,
+    'percenttotalsupply': 1262398128305471549
 }
+
+CATEGORY_ID = 1262397774692089899
 
 # Embedded message channel ID
 EMBED_CHANNEL_ID = 567890123
